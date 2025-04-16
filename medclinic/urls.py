2e61b1b8f3aeb03cf251597a9fef2ee0cfe7fc87
MariaIdk <taranma26agmail.com@yandex.ru>
2025-04-16 12:18:40 +0300

    Добавлены компоненты для загрузки и отображения документов на страницу пациента

--- medclinic/urls.py
+++ medclinic/urls.py
@@ -16,9 +16,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Добро пожаловать на главную страницу!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls')), 
+    path('api/', include('api.urls')),  # если есть, например, API
+    path('', home),  # добавляем маршрут для корня
 ]
 

e26c4572c79d68e5d185e6701f5543a06d0db6cd
MariaIdk <taranma26agmail.com@yandex.ru>
2025-06-11 10:30:41 +0300

    Добавлен стиль на форму приема

--- medclinic-frontend/src/components/common/Footer.js
+++ medclinic-frontend/src/components/common/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div>© 2025 MedClinic</div>
-      <div>Адрес: г. Москва, ул. Примерная, д.1</div>
+      <div>Адрес: г. Михайловск ул. Ленина д. 183</div>
       <div>Телефон: +7 (495) 123-45-67</div>
       <div>Email: info@medclinic.ru</div>
     </footer>
